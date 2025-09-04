81f6e92d7279850aa9a58a6afae59c2365303c84
Oleksandr Klumchyk <oleksandrpolyana@gmail.com>
2025-09-05 00:23:43 +0300

    Adedd 2 section

---
+++ Monticello-kontol_2/assets/Js/dist/main.dev.js
@@ -0,0 +1,41 @@
+"use strict";
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector(".header");
+
+  if (window.scrollY > 50) {
+    header.style.background = "#8464f6ff";
+  } else {
+    header.style.background = "transparent";
+  }
+});
+$(document).ready(function () {
+  var slider = $("#newsSlider").lightSlider({
+    item: 3,
+    loop: true,
+    auto: true,
+    slideMargin: 30,
+    pager: false,
+    controls: false,
+    speed: 800,
+    responsive: [{
+      breakpoint: 800,
+      settings: {
+        item: 2,
+        slideMove: 1
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        item: 1,
+        slideMove: 1
+      }
+    }]
+  });
+  $(".custom-prev").click(function () {
+    slider.goToPrevSlide();
+  });
+  $(".custom-next").click(function () {
+    slider.goToNextSlide();
+  });
+});
---
+++ Monticello-kontol_2/assets/Js/main.js
@@ -0,0 +1,32 @@
+window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    if (window.scrollY > 50) {
+        header.style.background = "#8464f6ff";
+    } else {
+        header.style.background = "transparent";
+    }
+});
+
+$(document).ready(function () {
+    const slider = $("#newsSlider").lightSlider({
+        item: 3,
+        loop: true,
+        auto: true,
+        slideMargin: 30,
+        pager: false,
+        controls: false,
+        speed: 800,
+        responsive: [
+            { breakpoint: 800, settings: { item: 2, slideMove: 1 } },
+            { breakpoint: 480, settings: { item: 1, slideMove: 1 } }
+        ]
+    });
+
+    $(".custom-prev").click(function () {
+        slider.goToPrevSlide();
+    });
+
+    $(".custom-next").click(function () {
+        slider.goToNextSlide();
+    });
+});
